457d148bea01753c6497f203d256fa996fa892df
Luj8n <l.milieska@gmail.com>
2021-10-21 23:01:07 +0300

    enabled dark mode and added primary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 // tailwind.config.js
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   mode: "jit",
   purge: ["{pages,app}/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.indigo,
+        secondary: colors.yellow,
+        neutral: colors.gray,
+      },
+    },
   },
   variants: {
     extend: {},
