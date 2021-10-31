b7a085525d6b1e600cfa3d1b86de729c194c079a
Luj8n <l.milieska@gmail.com>
2021-10-31 18:01:14 +0200

    reset password done

--- app/auth/pages/reset-password.tsx
+++ app/auth/pages/reset-password.tsx
@@ -9,51 +9,69 @@ const ResetPasswordPage: BlitzPage = () => {
   const query = useRouterQuery()
   const [resetPasswordMutation, { isSuccess }] = useMutation(resetPassword)
 
-  return (
-    <>
-      <h1>Set a New Password</h1>
+  if (isSuccess)
+    return (
+      <div className="flex items-center justify-center flex-col h-5/6 w-auto mx-auto">
+        <h1 className="mb-10 text-4xl font-bold text-center">Password Reset Successfully</h1>
+        <p className="text-base text-center text-primary-500 font-semibold hover:text-primary-400">
+          <Link href={Routes.Home()}>Go to the homepage</Link>
+        </p>
+      </div>
+    )
 
-      {isSuccess ? (
-        <div>
-          <h2>Password Reset Successfully</h2>
-          <p>
-            Go to the <Link href={Routes.Home()}>homepage</Link>
-          </p>
-        </div>
-      ) : (
-        <Form
-          submitText="Reset Password"
-          schema={ResetPassword}
-          initialValues={{ password: "", passwordConfirmation: "", token: query.token as string }}
-          onSubmit={async (values) => {
-            try {
-              await resetPasswordMutation(values)
-            } catch (error: any) {
-              if (error.name === "ResetPasswordError") {
-                return {
-                  [FORM_ERROR]: error.message,
-                }
-              } else {
-                return {
-                  [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
-                }
+  return (
+    <div className="flex items-center justify-center flex-col h-5/6 w-[28rem] mx-auto">
+      <h1 className="w-full mb-10 text-4xl font-bold text-center">Set a New Password</h1>
+
+      <Form
+        submitText="Reset Password"
+        schema={ResetPassword}
+        initialValues={{ password: "", passwordConfirmation: "", token: query.token as string }}
+        className="w-full rounded-lg shadow-md bg-neutral-50 dark:bg-neutral-700 p-9"
+        buttonProps={{
+          className:
+            "w-full rounded-md bg-primary-600 text-neutral-50 py-2.5 font-medium disabled:bg-primary-500 disabled:cursor-default hover:bg-primary-700 active:ring-4",
+        }}
+        alertProps={{ className: "text-red-600" }}
+        onSubmit={async (values) => {
+          try {
+            await resetPasswordMutation(values)
+          } catch (error: any) {
+            if (error.name === "ResetPasswordError") {
+              return {
+                [FORM_ERROR]: error.message,
+              }
+            } else {
+              return {
+                [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
               }
             }
-          }}
-        >
-          <LabeledTextField name="password" label="New Password" type="password" />
-          <LabeledTextField
-            name="passwordConfirmation"
-            label="Confirm New Password"
-            type="password"
-          />
-        </Form>
-      )}
-    </>
+          }
+        }}
+      >
+        <LabeledTextField
+          name="password"
+          label="New Password"
+          type="password"
+          className="appearance-none outline-none w-full text-base py-1.5 px-2 rounded-md border-2 border-opacity-50 border-neutral-400 focus:border-primary-500 border-solid dark:bg-neutral-600 dark:disabled:bg-neutral-500 bg-neutral-50 disabled:bg-neutral-200"
+          labelProps={{ className: "mb-1.5" }}
+          alertProps={{ className: "text-red-600 text-sm mt-1" }}
+          outerProps={{ className: "mb-3" }}
+        />
+        <LabeledTextField
+          name="passwordConfirmation"
+          label="Confirm New Password"
+          type="password"
+          className="appearance-none outline-none w-full text-base py-1.5 px-2 rounded-md border-2 border-opacity-50 border-neutral-400 focus:border-primary-500 border-solid dark:bg-neutral-600 dark:disabled:bg-neutral-500 bg-neutral-50 disabled:bg-neutral-200"
+          labelProps={{ className: "mb-1.5" }}
+          alertProps={{ className: "text-red-600 text-sm mt-1" }}
+          outerProps={{ className: "mb-3" }}
+        />
+      </Form>
+    </div>
   )
 }
 
-ResetPasswordPage.redirectAuthenticatedTo = Routes.Home()
 ResetPasswordPage.getLayout = (page) => <Layout title="Reset Your Password">{page}</Layout>
 
 export default ResetPasswordPage
