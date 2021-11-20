a508f5ee36a0ec97235cde1c4f6df6b47c927fd4
Luj8n <l.milieska@gmail.com>
2021-11-20 21:46:53 +0200

    more seeding

--- db/seeds.ts
+++ db/seeds.ts
@@ -14,8 +14,44 @@ const seed = async () => {
     data: {
       email: "admin@admin.com",
       hashedPassword: await SecurePassword.hash("admin"),
-      name: "SuperUser",
+      name: "Admin",
       role: "ADMIN",
+    },
+  })
+
+  const categoriesData = [
+    { name: "Strings", description: "You have to manipulate strings" },
+    { name: "Numbers", description: "You have to manipulate numbers" },
+    { name: "Recursion", description: "You have to use recursion" },
+  ]
+
+  for (const categoryData of categoriesData) {
+    await db.category.create({
+      data: {
+        ...categoryData,
+      },
+    })
+  }
+
+  await db.problem.create({
+    data: {
+      title: "Reverse a string",
+      description: "TODO",
+      difficulty: "EASY",
+      type: "IO",
+      visibility: "EVERYONE",
+      user: {
+        connect: { email: "admin@admin.com" },
+      },
+      categories: {
+        connect: [{ id: (await db.category.findFirst({ where: { name: "Strings" } }))?.id }],
+      },
+      likedBy: { connect: [{ email: "admin@admin.com" }] },
+      tests: {
+        createMany: {
+          data: [{ input: "abcdef", expectedOutput: "fedcba" }],
+        },
+      },
     },
   })
 }
